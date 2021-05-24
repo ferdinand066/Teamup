15d48e7962d1f6f965da62e3fe9f59efa65da131
ferdinand066 <ferdinandg066@gmail.com>
2021-05-24 11:21:52 +0700

    Migration make table

---
+++ TeamUp/database/migrations/2021_05_24_040635_create_team_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_details', function (Blueprint $table) {
+            $table->uuid('team_id');
+            $table->uuid('member_id');
+            $table->uuid('position_id');
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+            $table->foreign('member_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('position_id')->references('id')->on('positions')->onDelete('cascade');
+            $table->primary(array('team_id', 'member_id'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team_details');
+    }
+}
